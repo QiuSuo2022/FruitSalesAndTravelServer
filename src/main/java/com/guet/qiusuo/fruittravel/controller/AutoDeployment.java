e1454e3d0ba30a6de7df36d3f4c31cfde334d63e
李斌 <1347006947@qq.com>
2022-02-05 23:15:27 +0800

    update: test 自动部署

--- src/main/java/com/guet/qiusuo/fruittravel/controller/AutoDeployment.java
+++ src/main/java/com/guet/qiusuo/fruittravel/controller/AutoDeployment.java
@@ -3,16 +3,19 @@ package com.guet.qiusuo.fruittravel.controller;
 import com.guet.qiusuo.fruittravel.config.ErrorCode;
 import com.guet.qiusuo.fruittravel.config.Response;
 import com.guet.qiusuo.fruittravel.config.SystemException;
-import com.guet.qiusuo.fruittravel.utils.SHAUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.codec.binary.Hex;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @Author: libuyan
@@ -25,31 +28,44 @@ public class AutoDeployment {
 
     @ApiOperation(value = "执行自动更新脚本")
     @PostMapping("/update")
-    public Response autoUpdate(HttpServletRequest request) throws IOException {
+    public Response autoUpdate(HttpServletRequest request) {
         String headSignature256 = request.getHeader("X-Hub-Signature-256");
         System.out.println(headSignature256);
         String requestBody = getBodyStringByReader(request);
+        String genSignature256 = null;
         try {
-            String genSignature256 = SHAUtil.HMACSHA256(requestBody, "fruitSale123.");
+            genSignature256 = "sha256=" + HMACSHA256(requestBody, "fruitSale123.");
             System.out.println(genSignature256);
-            if (!genSignature256.equals(headSignature256)) {
-                throw new SystemException(ErrorCode.X_GITEE_TOKEN_MISMATCH);
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        if (!headSignature256.equals(genSignature256)) {
+            throw new SystemException(ErrorCode.X_GITEE_TOKEN_MISMATCH);
+        }
+
         Runtime run = Runtime.getRuntime();
-        Process exec = run.exec(new String[]{"/bin/sh", "-c", "cd /root/project/fruitSaleAndTravel && ./deploy.sh"});
+        try {
+            run.exec(new String[]{"/bin/sh", "-c", "cd /root/project/fruitSaleAndTravel && ./deploy.sh"});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Response<>(200, "自动部署成功", "");
     }
 
-    public static String getBodyStringByReader(HttpServletRequest request) {
+    /**
+     * 读取request中的body部分
+     *
+     * @param request HttpServletRequest
+     *
+     * @return body toString
+     */
+    private static String getBodyStringByReader(HttpServletRequest request) {
         BufferedReader bufferedReader = null;
         StringBuilder sb = new StringBuilder();
         try {
             bufferedReader = request.getReader();
-            String line = "";
+            String line;
             while ((line = bufferedReader.readLine()) != null) {
                 sb.append(line);
             }
@@ -65,5 +81,23 @@ public class AutoDeployment {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * 生成 HMACSHA256
+     *
+     * @param data 待处理数据
+     * @param key  密钥
+     *
+     * @return 加密结果
+     *
+     * @throws Exception
+     */
+    private static String HMACSHA256(String data, String key) throws Exception {
+        Mac sha256_HMAC = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256");
+        sha256_HMAC.init(secretKey);
+        byte[] hash = sha256_HMAC.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return Hex.encodeHexString(hash);
     }
 }
