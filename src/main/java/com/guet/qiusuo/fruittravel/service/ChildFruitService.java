a5afcb83c62571949ad8a4945bb75172c99fbe77
machine <l604636409@163.com>
2022-02-10 22:16:41 +0800

    update:更新增删改方法

--- src/main/java/com/guet/qiusuo/fruittravel/service/ChildFruitService.java
+++ src/main/java/com/guet/qiusuo/fruittravel/service/ChildFruitService.java
@@ -6,14 +6,21 @@ import com.guet.qiusuo.fruittravel.config.SystemException;
 import com.guet.qiusuo.fruittravel.config.UserContextHolder;
 import com.guet.qiusuo.fruittravel.dao.ChildFruitMapper;
 import com.guet.qiusuo.fruittravel.model.ChildFruit;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.UUID;
+import static org.slf4j.LoggerFactory.getLogger;
+import static java.lang.invoke.MethodHandles.lookup;
+
 @Service
 public class ChildFruitService {
+
     private ChildFruitMapper childFruitMapper;
+
+    private static final Logger LOG = getLogger(lookup().lookupClass());
 
     @Autowired
     public void setChildFruitMapper(ChildFruitMapper childFruitMapper){
@@ -25,17 +32,19 @@ public class ChildFruitService {
      * @param childFruit
      */
     public void addChildFruit(ChildFruit childFruit){
-        String userRoleId = UserContextHolder.getUser().getRoleId();
-        if (userRoleId.equals("1")){
-            throw new SystemException(ErrorCode.NO_PERMISSION);
-        }
+        UserContextHolder.validAdmin();
         long now = System.currentTimeMillis();
         childFruit.setId(UUID.randomUUID().toString());
         childFruit.setStatus(SystemConstants.STATUS_ACTIVE);
         childFruit.setCreateTime(now);
         childFruit.setUpdateTime(now);
-        childFruitMapper.insertSelective(childFruit);
-        System.out.println("创建水果子项成功:" + childFruit.toString());
+        childFruit.setCreateUserId(UserContextHolder.getUserId());
+        childFruit.setUpdateUserId(UserContextHolder.getUserId());
+        int i = childFruitMapper.insertSelective(childFruit);
+        if (i == 0){
+            throw new SystemException(ErrorCode.INSERT_ERROR);
+        }
+        LOG.info("创建水果子项成功,Id:{}",childFruit.getFruitId());
     }
 
     /**
@@ -43,15 +52,17 @@ public class ChildFruitService {
      * @param fruitId
      */
     public void deleteChildFruit(String fruitId){
-        String userRoleId = UserContextHolder.getUser().getRoleId();
-        if (userRoleId.equals("1")){
-            throw new SystemException(ErrorCode.NO_PERMISSION);
-        }
-        Optional<ChildFruit> optionalChildFruit = childFruitMapper.selectByPrimaryKey(fruitId);
+        UserContextHolder.validAdmin();
+        Optional<ChildFruit> optionalChildFruit = childFruitMapper.selectByFruitId(fruitId);
         ChildFruit childFruit = optionalChildFruit.orElseThrow(() -> new SystemException(ErrorCode.NO_FOUND_CHILD_FRUIT));
         childFruit.setStatus(SystemConstants.STATUS_NEGATIVE);
         childFruit.setUpdateTime(System.currentTimeMillis());
-        childFruitMapper.deleteByPrimaryKey(fruitId);
+        childFruit.setUpdateUserId(UserContextHolder.getUserId());
+        int i = childFruitMapper.updateByPrimaryKeySelective(childFruit);
+        if (i == 0){
+            throw new SystemException(ErrorCode.DELETE_ERROR);
+        }
+        LOG.info("删除水果子项成功,Id:{}",fruitId);
     }
 
     /**
@@ -59,16 +70,18 @@ public class ChildFruitService {
      * @param childFruit
      */
     public void updateChildFruit(ChildFruit childFruit){
-        String userRoleId = UserContextHolder.getUser().getRoleId();
-        if (userRoleId.equals("1")){
-            throw new SystemException(ErrorCode.NO_PERMISSION);
-        }
+        UserContextHolder.validAdmin();
         childFruit.setUpdateTime(System.currentTimeMillis());
-        childFruitMapper.updateByPrimaryKeySelective(childFruit);
+        childFruit.setUpdateUserId(UserContextHolder.getUserId());
+        int i = childFruitMapper.updateByPrimaryKeySelective(childFruit);
+        if (i == 0){
+            throw new SystemException(ErrorCode.UPDATE_ERROR);
+        }
+        LOG.info("修改水果子项成功,Id:{}",childFruit.getFruitId());
     }
 
     /**
-     * 查找childFruit
+     * 查找childFruit(status = SystemContext.STATUS_ACTIVE)
      * @param fruitId
      * @return
      */
