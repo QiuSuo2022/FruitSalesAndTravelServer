c10d6c262943b6cea7bcd2a4d1cb684373ec720a
dowelldoss <768823762@qq.com>
2022-02-12 23:11:14 +0800

    update:增加注解

--- src/main/java/com/guet/qiusuo/fruittravel/dao/TicketMapper.java
+++ src/main/java/com/guet/qiusuo/fruittravel/dao/TicketMapper.java
@@ -33,8 +33,10 @@ import org.mybatis.dynamic.sql.update.UpdateModel;
 import org.mybatis.dynamic.sql.update.render.UpdateStatementProvider;
 import org.mybatis.dynamic.sql.util.SqlProviderAdapter;
 import org.mybatis.dynamic.sql.util.mybatis3.MyBatis3Utils;
+import org.springframework.stereotype.Repository;
 
 @Mapper
+@Repository
 public interface TicketMapper {
     @Generated(value = "org.mybatis.generator.api.MyBatisGenerator", date = "2022-01-26T02:15:00.29+08:00", comments
             = "Source Table: tbl_ticket")
