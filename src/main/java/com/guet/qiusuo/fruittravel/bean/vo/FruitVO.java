278ba4b810a24bc55f7b16e5816cbe9688b7a33f
dowelldoss <768823762@qq.com>
2022-02-25 16:06:11 +0800

    add：水果VO类

---
+++ src/main/java/com/guet/qiusuo/fruittravel/bean/vo/FruitVO.java
@@ -0,0 +1,14 @@
+package com.guet.qiusuo.fruittravel.bean.vo;
+
+import com.guet.qiusuo.fruittravel.model.Fruit;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class FruitVO extends Fruit {
+
+    @ApiModelProperty(value = "水果库存数量")
+    private Integer stock;
+}
