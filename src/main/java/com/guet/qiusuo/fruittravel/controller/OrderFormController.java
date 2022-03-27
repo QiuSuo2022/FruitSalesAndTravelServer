0938958cf59c03fa3386c9b903d4631e695a827b
machine <l604636409@163.com>
2022-03-27 23:07:40 +0800

    add: 新增订单功能 OrderFormController与OrderFormService

---
+++ src/main/java/com/guet/qiusuo/fruittravel/controller/OrderFormController.java
@@ -0,0 +1,16 @@
+package com.guet.qiusuo.fruittravel.controller;
+
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : lu
+ */
+@Api(tags = "订单")
+@RestController
+@RequestMapping("/OrderForm")
+public class OrderFormController {
+
+}
---
+++ src/main/java/com/guet/qiusuo/fruittravel/service/OrderFormService.java
@@ -0,0 +1,8 @@
+package com.guet.qiusuo.fruittravel.service;
+
+/**
+ * @author : lu
+ */
+public class OrderFormService {
+
+}
