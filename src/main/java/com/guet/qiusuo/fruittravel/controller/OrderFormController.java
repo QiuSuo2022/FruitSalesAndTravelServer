3bfb17243065d72290fbd09e4eb05622fa301995
machine <l604636409@163.com>
2022-03-30 21:06:55 +0800

    add: 增加订单的基本方法

--- src/main/java/com/guet/qiusuo/fruittravel/controller/OrderFormController.java
+++ src/main/java/com/guet/qiusuo/fruittravel/controller/OrderFormController.java
@@ -1,7 +1,16 @@
 package com.guet.qiusuo.fruittravel.controller;
 
 
+import com.guet.qiusuo.fruittravel.config.ErrorCode;
+import com.guet.qiusuo.fruittravel.config.SystemException;
+import com.guet.qiusuo.fruittravel.model.OrderForm;
+import com.guet.qiusuo.fruittravel.service.OrderFormService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/OrderForm")
 public class OrderFormController {
 
+    private OrderFormService orderFormService;
+
+    @Autowired
+    public void setOrderFormService(OrderFormService orderFormService) {
+        this.orderFormService = orderFormService;
+    }
+
+    @ApiOperation(value = "创建预付订单")
+    @PostMapping("/createOrder")
+    public OrderForm createOrder (@RequestBody OrderForm order){
+        if (order == null){
+            throw new SystemException(ErrorCode.PARAM_NULL_ERROR);
+        }
+        return orderFormService.createOrderForm(order);
+    }
+
+    @ApiOperation(value = "删除订单")
+    @RequestMapping("/deleteOrder")
+    public boolean deleteOrder(@Parameter String orderId){
+        return orderFormService.deleteOrderForm(orderId);
+    }
+
+    @ApiOperation(value = "回调接口")
+    @RequestMapping("/notify")
+    public String orderNotify(){
+        return "";
+    }
 }
--- src/main/java/com/guet/qiusuo/fruittravel/service/OrderFormService.java
+++ src/main/java/com/guet/qiusuo/fruittravel/service/OrderFormService.java
@@ -1,8 +1,66 @@
 package com.guet.qiusuo.fruittravel.service;
 
+import com.guet.qiusuo.fruittravel.common.SystemConstants;
+import com.guet.qiusuo.fruittravel.config.ErrorCode;
+import com.guet.qiusuo.fruittravel.config.SystemException;
+import com.guet.qiusuo.fruittravel.config.UserContextHolder;
+import com.guet.qiusuo.fruittravel.dao.OrderFormMapper;
+import com.guet.qiusuo.fruittravel.model.OrderForm;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.lang.invoke.MethodHandles.lookup;
+import static org.slf4j.LoggerFactory.getLogger;
+
 /**
  * @author : lu
  */
+@Service
 public class OrderFormService {
 
+    private OrderFormMapper orderFormMapper;
+    private static final Logger LOG = getLogger(lookup().lookupClass());
+
+    @Autowired
+    public void setOrderFormMapper(OrderFormMapper orderFormMapper) {
+        this.orderFormMapper = orderFormMapper;
+    }
+
+    /**
+     * 创建订单
+     * @param orderForm
+     * @return
+     */
+    public OrderForm createOrderForm(OrderForm orderForm){
+        long now = System.currentTimeMillis();
+        orderForm.setId(UUID.randomUUID().toString().replace("-", ""));
+        orderForm.setCreateTime(now);
+        orderForm.setUpdateTime(now);
+        orderForm.setCreateUserId(UserContextHolder.getUserId());
+        orderForm.setUpdateUserId(UserContextHolder.getUserId());
+        orderForm.setOrderFormStatus(SystemConstants.UNPAID);
+        orderForm.setStatus(SystemConstants.STATUS_ACTIVE);
+        int i = orderFormMapper.insert(orderForm);
+        if (i == 0){
+            throw new SystemException(ErrorCode.INSERT_ERROR);
+        }
+        return orderForm;
+    }
+
+    /**
+     * 删除订单
+     * @return
+     */
+    public boolean deleteOrderForm(String orderFormId){
+        Optional<OrderForm> orderFormOptional = orderFormMapper.selectByPrimaryKey(orderFormId);
+        OrderForm orderForm = orderFormOptional.orElseThrow(() -> new SystemException(ErrorCode.DELETE_ERROR));
+        orderForm.setUpdateTime(System.currentTimeMillis());
+        orderForm.setUpdateUserId(UserContextHolder.getUserId());
+        orderForm.setStatus(SystemConstants.STATUS_NEGATIVE);
+        return true;
+    }
 }
