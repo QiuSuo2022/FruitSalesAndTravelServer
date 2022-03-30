ed3309a8ba486ade0b37c2743fc2f71f2f7f94fd
dowelldoss <768823762@qq.com>
2022-03-30 18:21:03 +0800

    fix:修改异常

--- src/main/java/com/guet/qiusuo/fruittravel/dao/FruitMapper.java
+++ src/main/java/com/guet/qiusuo/fruittravel/dao/FruitMapper.java
@@ -298,7 +298,7 @@ public interface FruitMapper {
             "on tbl_child_fruit.id = tbl_cart.child_fruit_id",
             "left join tbl_evaluate",
             "on tbl_child_fruit.id = tbl_evaluate.product_id",
-            "where tbl_fruit.fruit_name like %#{nameLike}%",
+            "where tbl_fruit.fruit_name like ’%#{nameLike}%‘",
             "order by (tbl_child_fruit.fruit_price*0.25 + quantity*0.5 + grade*0.25)"
     })
     @Results(id = "SortResult", value = {
@@ -322,7 +322,7 @@ public interface FruitMapper {
             "on tbl_cart.child_fruit_id = tbl_child_fruit.id",
             "left join tbl_evaluate",
             "on tbl_child_fruit.id = tbl_evaluate.product_id",
-            "where tbl_child_fruit.fruit_name like %#{nameLike}%",
+            "where tbl_child_fruit.fruit_name like ’%#{nameLike}%‘",
             "group by tbl_child_fruit.fruit_name,description,departure_point,quantity,grade,tbl_child_fruit.fruit_price",
             "order by tbl_child_fruit.fruit_price*0.25 + quantity*0.5 + grade*0.25"
     })
