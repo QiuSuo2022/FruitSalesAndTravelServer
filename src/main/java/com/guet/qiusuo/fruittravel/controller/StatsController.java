738532323c4ef1fe940226073c4b3b296587129d
machine <l604636409@163.com>
2022-03-30 21:07:39 +0800

    update: 校验用户权限

--- src/main/java/com/guet/qiusuo/fruittravel/controller/StatsController.java
+++ src/main/java/com/guet/qiusuo/fruittravel/controller/StatsController.java
@@ -1,4 +1,5 @@
 package com.guet.qiusuo.fruittravel.controller;
+import com.guet.qiusuo.fruittravel.config.UserContextHolder;
 import com.guet.qiusuo.fruittravel.service.StatsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -33,12 +34,14 @@ public class StatsController {
     @ApiOperation(value = "获取销量最高的水果")
     @GetMapping("/getTopSalesFruit")
     public HashMap<String,Long> getTopSaleFruit(@RequestParam short ago){
+        UserContextHolder.validAdmin();
         return statsService.getTopSaleFruit(ago);
     }
 
     @ApiOperation(value = "获取一种水果一周/月/年的销量")
     @GetMapping("/getOneFruitSales")
     public long getOneFruitSalesByFruitId(@RequestParam String fruitId,@RequestParam short ago){
+        UserContextHolder.validAdmin();
         long amount = 0;
         amount = statsService.getSingleSalesByFruitId(fruitId, ago);
         return amount;
@@ -47,13 +50,15 @@ public class StatsController {
     @ApiOperation(value = "获取所有水果一周/月/年的销量的报表数据")
     @GetMapping("/getAllFruitSalesData")
     public List<Map.Entry<String, Long>> getAllFruitSalesData(@RequestParam short ago){
+        UserContextHolder.validAdmin();
         return statsService.getAllFruitsSalesByAgo(ago);
     }
 
     @ApiOperation(value = "获取所有水果一周/月/年的评价的报表")
     @GetMapping("/getAllFruitEvalData")
     public List<Map.Entry<String, Long>> getAllFruitEvalData(@RequestParam short ago, short evaluationType){
-       return statsService.getAllFruitsEvaluationByAgo(ago,evaluationType);
+        UserContextHolder.validAdmin();
+        return statsService.getAllFruitsEvaluationByAgo(ago,evaluationType);
     }
 
     /**
@@ -62,12 +67,14 @@ public class StatsController {
     @ApiOperation(value = "获取销量最高的景区")
     @GetMapping("/getTopSalesScenic")
     public HashMap<String,Long> getTopSaleScenic(@RequestParam short ago){
+        UserContextHolder.validAdmin();
         return statsService.getTopSaleScenic(ago);
     }
 
     @ApiOperation(value = "获取一种景区一周/月/年的销量")
     @GetMapping("/getOneScenicSales")
     public long getSalesAmountByScenicId(@RequestParam String scenicId,@RequestParam short ago){
+        UserContextHolder.validAdmin();
         long amount = 0;
         amount = statsService.getSingleSalesByScenicId(scenicId,ago);
         return amount;
@@ -76,12 +83,14 @@ public class StatsController {
     @ApiOperation(value = "获取所有景区一周/月/年的销量的报表")
     @GetMapping("/getAllScenicSalesData")
     public List<Map.Entry<String, Long>> getAllScenicSalesData(@RequestParam short ago){
+        UserContextHolder.validAdmin();
         return statsService.getAllScenicSalesByAgo(ago);
     }
 
     @ApiOperation(value = "获取所有景区一周/月/年的评价的报表")
     @GetMapping("/getAllScenicEvalData")
     public List<Map.Entry<String, Long>> getAllScenicEvalData(@RequestParam short ago, short evaluationType){
+        UserContextHolder.validAdmin();
         return statsService.getAllScenicEvaluationByAgo(ago,evaluationType);
     }
 }
