4b17886bf43ac8a1053f169717840f11e038e879
machine <l604636409@163.com>
2022-04-10 17:57:30 +0800

    update: 整合sql脚本

--- src/main/resources/db/migration/V0.0.1__init.sql
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -231,3 +231,48 @@ create table tbl_role_permission
     constraint tbl_role_permission_create_user_id_fk foreign key (create_user_id) references tbl_user (id),
     constraint tbl_role_permission_update_user_id_fk foreign key (update_user_id) references tbl_user (id)
 ) comment '角色权限表';
+create table tbl_user_role
+(
+    id              varchar(44) not null comment 'uuid',
+    role_id         varchar(44) not null comment '角色id',
+    user_id         varchar(44) not null comment '用户id',
+    status          SMALLINT comment '状态,0:禁用 1:启用',
+    create_time     BIGINT comment '创建时间',
+    update_time     BIGINT comment '更新时间',
+    create_user_id  VARCHAR(44) comment '记录创建人ID',
+    update_user_id  VARCHAR(44) comment '记录修改人ID',
+    constraint tbl_user_role_pk primary key (id),
+    constraint tbl_user_role_role_id_fk foreign key (role_id) references tbl_role (id),
+    constraint tbl_user_role_user_id_fk foreign key (user_id) references tbl_user (id),
+    constraint tbl_user_role_create_user_id_fk foreign key (create_user_id) references tbl_user (id),
+    constraint tbl_user_role_update_user_id_fk foreign key (update_user_id) references tbl_user (id)
+) comment '角色权限表';
+CREATE TABLE tbl_order_form
+(
+    id                  VARCHAR(44) NOT NULL COMMENT '订单编号uuid',
+    address             VARCHAR(255)  COMMENT '收货地址',
+    express             VARCHAR(127) COMMENT '物流信息',
+    name                varchar (64) COMMENT '产品名称',
+    fruit_id            VARCHAR(44)  COMMENT '水果id',
+    scenic_id 	    	VARCHAR(44)  COMMENT '景区id',
+    amount              INT(11) NULL COMMENT '数目',
+    fee                 INT(11) NULL COMMENT '实际付款金额(单位:分)',
+    pay_status          SMALLINT  NOT NULL COMMENT '订单状态:未支付-0 已支付-1 待发货-2 已发货-3 已完成-4 已退款-5',
+    has_evaluate        SMALLINT  COMMENT '是否已经评价:未评价-0 已评价-1',
+    bind_evaluate_id    VARCHAR(44)  COMMENT '评价表的id',
+    STATUS              SMALLINT COMMENT '状态,0:禁用 1:启用',
+    pay_time        BIGINT COMMENT '支付时间',
+    create_time     BIGINT COMMENT '创建时间',
+    update_time     BIGINT COMMENT '更新时间',
+    create_user_id  VARCHAR(44) NULL COMMENT '记录创建人ID',
+    update_user_id  VARCHAR(44) NULL COMMENT '记录修改人ID',
+    CONSTRAINT tbl_order_form_pk PRIMARY KEY (id),
+    CONSTRAINT tbl_order_form_create_user_id_fk FOREIGN KEY (create_user_id) REFERENCES tbl_user (id),
+    CONSTRAINT tbl_order_form_update_user_id_fk FOREIGN KEY (update_user_id) REFERENCES tbl_user (id),
+    CONSTRAINT tbl_order_form_bind_evaluate_id_fk FOREIGN KEY (bind_evaluate_id) REFERENCES tbl_evaluate (id),
+    CONSTRAINT tbl_order_form_fruit_id_fk FOREIGN KEY (fruit_id) REFERENCES tbl_fruit (id),
+    CONSTRAINT tbl_order_form_scenic_id_fk FOREIGN KEY (scenic_id) REFERENCES tbl_scenic (id)
+) COMMENT '订单表';
+
+
+
