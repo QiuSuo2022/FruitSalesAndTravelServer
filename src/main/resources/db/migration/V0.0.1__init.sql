bdebd01a6dc8c7f739801325fe6145c7bcf63387
dowelldoss <768823762@qq.com>
2022-05-01 23:36:05 +0800

    add:增加goods表

--- src/main/resources/db/migration/V0.0.1__init.sql
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -252,11 +252,6 @@ CREATE TABLE tbl_order_form
     id                  VARCHAR(44) NOT NULL COMMENT '订单编号uuid',
     address             VARCHAR(255)  COMMENT '收货地址',
     express             VARCHAR(127) COMMENT '物流信息',
-    name                varchar (64) COMMENT '商品名',
-    price               INT(11) NULL COMMENT '商品单价',
-    fruit_id            VARCHAR(44)  COMMENT '水果id',
-    scenic_id 	    	VARCHAR(44)  COMMENT '景区id',
-    amount              INT(11) NULL COMMENT '数目',
     fee                 INT(11) NULL COMMENT '实际付款金额(单位:分)',
     pay_status          SMALLINT  NOT NULL COMMENT '订单状态:未支付-0 已支付-1 待发货-2 已发货-3 已完成-4 已退款-5',
     has_evaluate        SMALLINT  COMMENT '是否已经评价:未评价-0 已评价-1',
@@ -270,9 +265,7 @@ CREATE TABLE tbl_order_form
     CONSTRAINT tbl_order_form_pk PRIMARY KEY (id),
     CONSTRAINT tbl_order_form_create_user_id_fk FOREIGN KEY (create_user_id) REFERENCES tbl_user (id),
     CONSTRAINT tbl_order_form_update_user_id_fk FOREIGN KEY (update_user_id) REFERENCES tbl_user (id),
-    CONSTRAINT tbl_order_form_bind_evaluate_id_fk FOREIGN KEY (bind_evaluate_id) REFERENCES tbl_evaluate (id),
-    CONSTRAINT tbl_order_form_fruit_id_fk FOREIGN KEY (fruit_id) REFERENCES tbl_fruit (id),
-    CONSTRAINT tbl_order_form_scenic_id_fk FOREIGN KEY (scenic_id) REFERENCES tbl_scenic (id)
+    CONSTRAINT tbl_order_form_bind_evaluate_id_fk FOREIGN KEY (bind_evaluate_id) REFERENCES tbl_evaluate (id)
 ) COMMENT '订单表';
 
 create table tbl_deliveryInfo
