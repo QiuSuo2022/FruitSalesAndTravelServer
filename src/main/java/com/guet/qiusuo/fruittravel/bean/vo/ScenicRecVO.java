12c489ee1fdb5a44001cd1a01efe9cdc1b280f70
_Machine <Onlybmachine@hotmail.com>
2022-05-15 00:11:50 +0800

    update: vo类内容修改

--- src/main/java/com/guet/qiusuo/fruittravel/bean/vo/ScenicRecVO.java
+++ src/main/java/com/guet/qiusuo/fruittravel/bean/vo/ScenicRecVO.java
@@ -5,11 +5,15 @@ import lombok.Data;
 
 @Data
 public class ScenicRecVO {
+
+    @ApiModelProperty(value = "景区id")
+    private String ScenicId;
+
     @ApiModelProperty(value = "景区名")
     private String scenicName;
 
     @ApiModelProperty(value = "门票价格")
-    private String ticketPrice;
+    private Integer ticketPrice;
 
     @ApiModelProperty(value = "景区销量")
     private Integer sales;
